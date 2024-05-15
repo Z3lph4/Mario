51f45e6c464b9e1dc12693a9a6c09854c3e696a1
Z3lph4 <martimfasantiago@gmail.com>
2024-05-15 22:00:15 +0100

    teste

---
+++ js/start-menu.js
@@ -0,0 +1,5 @@
+const startButton = document.getElementById('start-button');
+
+startButton.addEventListener('click', () => {
+    window.location.href = "./main-game.html"; // Redireciona para a página do jogo (main-game.html)
+});
